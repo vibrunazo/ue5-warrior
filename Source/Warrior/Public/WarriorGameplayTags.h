30aacbe3ffff804ed94ef8b42e22d5e579422669
vib <vibrunazo@gmail.com>
2024-07-28 00:14:10 -0300

    11. Native Gameplay Tags

---
+++ Source/Warrior/Public/WarriorGameplayTags.h
@@ -0,0 +1,13 @@
+// vibrunazo
+
+#pragma once
+
+#include "NativeGameplayTags.h"
+
+namespace WarriorGameplayTags
+{
+	/** Input Tags **/
+	WARRIOR_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(InputTag_Move);
+	WARRIOR_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(InputTag_Look);
+	
+}
